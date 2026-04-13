220de5b39c6710b0e77de04de948f25f4eca8b51
gjoson <jijo4u@live.com>
2026-04-14 01:52:16 +0530

    Create app/build.gradle.kts for Android SDK 34 configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.gjoson.termkey"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.gjoson.termkey"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.9.0")
+}
